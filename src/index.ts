65d9a1de30055d62b02e1760c0044a187a7d3cea
James <pawson1988@me.com>
2023-03-03 22:14:44 +0100

    added method to add class and converted to non static class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { inputForm } from "./inputForm.js"
 
-inputForm.addFormAndInputsToBody()
 
 
-inputForm.inputTax.addEventListener("input", function(){
-    console.log(inputForm.getInputTax())
-})
 
+const form = new inputForm()
+form.addFormAndInputsToBody()
+
+form.addListenerInputTax()
+
+form.setClassNameOnElement("tax", "tax")
+form.setClassNameOnElement("price", "price")
+form.setClassNameOnElement("item", "item")
+form.setClassNameOnElement("company name", "company-name")
+form.setClassNameOnElement("form", "form")
 
--- src/inputForm.ts
+++ src/inputForm.ts
@@ -1,23 +1,29 @@
-
+type ElementsTypes = "tax" | "price" | "item" | "company name" | "form"
 
 export class inputForm {
-    static body = document.querySelector("body")!
-    static form = document.createElement("form")
-    static inputPrice = document.createElement("input")
-    static inputCompanyName = document.createElement("input")
-    static inputItem = document.createElement("input")
-    static inputTax = document.createElement("input")
-    static inputTotalAmount = document.createElement("p")
 
-    static inputPriceLabel = document.createElement("label")
-    static inputCompanyNameLabel = document.createElement("label")
-    static inputItemLabel = document.createElement("label")
-    static inputTaxLabel = document.createElement("label")
-    static inputTotalAmountLabel = document.createElement("label")
+    // create HTML elements
+    private body = document.querySelector("body")!
+    private form = document.createElement("form")
+    private inputPrice = document.createElement("input")
+    private inputCompanyName = document.createElement("input")
+    private inputItem = document.createElement("input")
+    private inputTax = document.createElement("input")
+    private inputTotalAmount = document.createElement("p")
+
+    // Create HTML labels for inputs
+    private inputPriceLabel = document.createElement("label")
+    private inputCompanyNameLabel = document.createElement("label")
+    private inputItemLabel = document.createElement("label")
+    private inputTaxLabel = document.createElement("label")
+    private inputTotalAmountLabel = document.createElement("label")
+
+    constructor(){
+        console.log("form initiated")
+    }
     
 
-    static addFormAndInputsToBody(){
-
+    addFormAndInputsToBody(){
         // add attributes to form elements 
         this.inputPrice.setAttribute("type", "number")
         this.inputCompanyName.setAttribute("type", "text")
@@ -42,6 +48,7 @@ export class inputForm {
         this.inputItemLabel.textContent = "Item"
         this.inputTaxLabel.textContent = "Tax"
 
+        //add child elements to form
         this.form.appendChild(this.inputTotalAmount)
         this.form.appendChild(this.inputTaxLabel)
         this.form.appendChild(this.inputTax)
@@ -52,11 +59,28 @@ export class inputForm {
         this.form.appendChild(this.inputPriceLabel)
         this.form.appendChild(this.inputPrice)
         
+        //add form to body
         this.body.appendChild(this.form)
     }
 
-    static getInputTax(): number {
-        return this.inputTax.valueAsNumber
+    // test method 
+    addListenerInputTax(){
+        this.inputTax.addEventListener("input", function(){
+            console.log(this.valueAsNumber)
+        })
     }
+
+    setClassNameOnElement(element: ElementsTypes, className: string){
+        const elements = {
+            "tax": this.inputTax,
+            "price": this.inputPrice,
+            "item": this.inputItem,
+            "company name": this.inputCompanyName,
+            "form": this.form
+        }
+        elements[element].setAttribute("class", className)
+    }
+
+    
 
 }
